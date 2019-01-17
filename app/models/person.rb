f35f6a13c46de79e5c09e7678145fe4915eba4e3
Joy Clark <joy.clark@innoq.com>
2019-01-17 21:55:39 +0100

    Tests und arbeiten an Filter Methode

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+  def self.filter(name: nil, email: nil)
+    name = name ? "%#{name.downcase}%" : "%"
+    email = email ? "%#{email.downcase}%" : "%"
+
+    Person.where("lower(name) LIKE ?", name)
+      .where("lower(name) LIKE ?", email)
+  end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -2,6 +2,64 @@ require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
   test "seeding db worked" do
-    Person.all.size > 1
+    assert Person.all.size > 1
+  end
+
+  test "filter db by name" do
+    person = Person.new(name: "Joel Ju")
+    person.save
+    people = Person.where(["lower(name) LIKE :name", { name: "Joel Ju" }])
+    assert(people.first.name == "Joel Ju")
+  end
+
+  test "filter db by partial name" do
+    person = Person.new(name: "Joel Ju2")
+    person.save
+    people = Person.where("lower(name) LIKE ?", "%oe%")
+    people.each do |p|
+      assert p.name.downcase.include? "oe"
+    end
+  end
+
+  test "filter db by email" do
+    person = Person.new(email: "joey@boo.com")
+    person.save
+    people = Person.where("lower(email) LIKE ?", "%boo%")
+    people.each do |p|
+      assert p.email.downcase.include? "boo"
+    end
+  end
+
+  test "filter db by both email and name" do
+    person = Person.new(name: "joy", email: "joey@boo.com")
+    person.save
+    people = Person.where("lower(name) LIKE ?", "%jo%")
+      .where("lower(email) LIKE ?", "%boo%")
+    people.each do |p|
+      assert p.name.downcase.include? "jo"
+      assert p.email.downcase.include? "boo"
+    end
+  end
+
+  test "filter db by both email and name with null?" do
+    Person.new(name: "joy", email: "joey@boo.com").save
+    Person.new(name: "bob", email: "jo@foo.com").save
+    people = Person.where("lower(name) LIKE ?", "%")
+      .where("lower(email) LIKE ?", "%boo%")
+    people.each do |p|
+      assert p.name.downcase.include? "jo"
+      assert p.email.downcase.include? "boo"
+    end
+  end
+
+  test "filter by name in Person" do
+    Person.new(name: "Jooo", email: "aaa@bbb.com").save
+    Person.new(name: "Foop", email: "ccc@ddd.com").save
+
+    people = Person.filter(name: "oo")
+    assert people.size >= 2
+    people.each do |p|
+      assert p.name.downcase.include? "oo"
+    end
   end
 end
