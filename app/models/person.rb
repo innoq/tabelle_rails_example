9d1dfa59b4f346fe01a363888eeacae30bc7cf66
Joy Clark <joy.clark@innoq.com>
2019-02-08 14:35:10 +0100

    Filter Function for Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,24 @@
 class Person < ApplicationRecord
-  def self.filter(name: nil, email: nil, phone: nil)
+  def self.filter(name: nil, email: nil, phone: nil, address: nil)
     name = name ? "%#{name.downcase}%" : "%"
     email = email ? "%#{email.downcase}%" : "%"
     phone = phone ? "%#{phone.downcase}%" : "%"
+    address = address ? "%#{address.downcase}%" : "%"
 
     Person.where("lower(name) LIKE ?", name)
       .where("lower(email) LIKE ?", email)
       .where("lower(phone) LIKE ?", phone)
+      .where("lower(street) LIKE ? OR lower(city) LIKE ? OR lower(zip) LIKE ? OR lower(country) LIKE ?",
+        address, address, address, address)
+  end
+
+  def address_includes?(str)
+    str = str ? str.downcase : ""
+    street = self.street ? self.street.downcase : ""
+    city = self.city ? self.city.downcase : ""
+    zip = self.zip ? self.zip.downcase : ""
+    country = self.country ? self.country.downcase : ""
+
+    street.include?(str) || city.include?(str) || zip.include?(str) || country.include?(str)
   end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -24,4 +24,56 @@ class PersonTest < ActiveSupport::TestCase
       assert p.phone.downcase.include? "3"
     end
   end
+
+  test "address includes for street" do
+    person = Person.new(street: "foo")
+    assert person.address_includes? "Fo"
+  end
+
+  test "address includes for city" do
+    person = Person.new(city: "Boo")
+    assert person.address_includes? "boo"
+  end
+
+  test "address includes for zip" do
+    person = Person.new(zip: "99999")
+    assert person.address_includes? "99"
+  end
+
+  test "address includes for country" do
+    person = Person.new(country: "USA")
+    assert person.address_includes? "sa"
+  end
+
+  test "filter by street in address for Person" do
+    people = Person.filter(address: "Str")
+    assert people.size == 3
+    people.each do |p|
+      assert p.address_includes? "Str"
+    end
+  end
+
+  test "filter by city in address for Person" do
+    people = Person.filter(address: "City")
+    assert people.size == 2
+    people.each do |p|
+      assert p.address_includes? "City"
+    end
+  end
+
+  test "filter by zip in address for Person" do
+    people = Person.filter(address: "1234")
+    assert people.size == 2
+    people.each do |p|
+      assert p.address_includes? "1234"
+    end
+  end
+
+  test "filter by country in address for Person" do
+    people = Person.filter(address: "Erma")
+    assert people.size == 1
+    people.each do |p|
+      assert p.address_includes? "Erma"
+    end
+  end
 end
